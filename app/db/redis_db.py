6f11a9aaa1e2cd6892efb2d7f14d77c74a4c3743
ericous <0xinfronex@gmail.com>
2026-01-17 14:16:39 +0530

    commit

--- app/db/redis_db.py
+++ app/db/redis_db.py
@@ -1,9 +1,15 @@
-import redis
+from redis.asyncio import Redis
 from app.config.settings import REDIS_HOST, REDIS_PORT, REDIS_PASS
 
-cache = redis.Redis(
+cache: Redis = Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
     password=REDIS_PASS,
-    decode_responses=True
+    decode_responses=True,
+
+    socket_timeout=3,
+    socket_connect_timeout=3,
+    retry_on_timeout=True,
+    health_check_interval=30,
+    max_connections=20,
 )
